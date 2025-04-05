697e4fc3ec7b79fafba8b4caa42ecc126c03e6b9
Gahan Kotebagilu <Gahan.Kotebagilu@anthology.com>
2025-04-05 16:04:03 +0530

    Add employee management components and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './pages/employee-form/employee-form.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:'employee-list',
+        pathMatch: 'full'
+    },
+    {
+        path: 'employee-list',
+        component:EmployeeListComponent
+    },
+    {
+        path: 'create-employee',
+        component:EmployeeFormComponent
+    },
+    {
+        path: '**',
+        component:NotFoundComponent
+    }
+];
